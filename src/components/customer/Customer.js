4e293fd0ac8ba79f3b322ae43b986bc2b37f39d0
michaelc321 <michaelc321@yahoo.com>
2020-09-02 12:52:47 -0500

    basic layout

---
+++ src/components/customer/Customer.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Customer.css";
+
+export const Customer = () => (
+    <section className="customer">
+        <h3 className="customer__name">Customer Name</h3>
+        <div className="customer__breed">Premium</div>
+    </section>
+)
---
+++ src/components/location/Location.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Location.css"
+
+export const Location = () => (
+    <section className="location">
+        <h3 className="location__name">Where</h3>
+        <div className="location__breed">zip</div>
+    </section>
+)
